ae27763f8f0ad3efa46599b7b07a9d8338b6d247
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2021-03-22 10:24:00 -0300

    Update tp.js

--- tp.js
+++ tp.js
@@ -37,14 +37,14 @@ setInterval(tp,1000);
 
 
 
-var tamanho = 10;
+var t = 13;
 function fonte(){
     let tabela = document.getElementsByTagName("table");
     let tr = tabela[0].getElementsByTagName("tr");
     for(let x = 0; x < tr.length; x++){
     let td = tr[x].getElementsByTagName("td");
         for(let i = 0; i < td.length; i++){td[i].style = "";
-            td[i].style.fontSize = tamanho+"px";
+            td[i].style.fontSize = t+"px";
         }
     }
 }
