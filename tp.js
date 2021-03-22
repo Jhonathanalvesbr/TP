38a73b9233023b7f73b0bc373bdb3e9f13c69970
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2021-03-22 09:29:10 -0300

    Create tp.js

---
+++ tp.js
@@ -0,0 +1,35 @@
+function tp(){
+    let lauda = document.getElementsByClassName("ant-table-row");
+    let existe = "nao";
+    for(let x = 0; x < lauda .length; x++){
+    	let tp = lauda[x].getElementsByClassName("ant-table-cell")[9].innerText;
+    	if(tp == "TP"){
+	    existe = "sim";
+	    break;
+    	}
+    }
+    for(let x = 0; x < lauda .length; x++){
+        let tp = lauda[x].getElementsByClassName("ant-table-cell")[9].innerText;
+	let merchan = lauda[x].getElementsByClassName("ant-table-cell")[2].innerText;
+        if(existe == "sim" && tp != "TP" && merchan != "MERC"){
+            lauda[x].style.backgroundColor = "red";
+        }
+	if(merchan == "MERC"){
+	    lauda[x].style.backgroundColor = "green";
+	}
+        if(tp == "TP"){
+	    for(x; x < lauda.length; x++){
+		let stylee = lauda[x].style.backgroundColor;
+	        let merchan = lauda[x].getElementsByClassName("ant-table-cell")[2].innerText;
+	        if(merchan == "MERC"){
+	            lauda[x].style.backgroundColor = "green";
+	        }
+		if(stylee == "red"){
+		    lauda[x].style.backgroundColor = lauda[x-1].style.backgroundColor;
+		}
+            }
+            break;
+        }
+    }
+}
+setInterval(tp,1000);
