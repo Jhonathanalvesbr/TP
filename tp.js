f6eb0c5e708b96e6fe75db208e18377183210b75
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2021-03-22 10:09:11 -0300

    Update tp.js

--- tp.js
+++ tp.js
@@ -38,12 +38,12 @@ setInterval(tp,1000);
 
 
 
-function fonte(let fonte){
+function fonte(fonte){
     let tabela = document.getElementsByTagName("table");
     let tr = tabela[0].getElementsByTagName("tr");
     for(let x = 0; x < tr.length; x++){
     let td = tr[x].getElementsByTagName("td");
-        for(let i=0; i < td.length; i++){
+        for(let i = 0; i < td.length; i++){td[i].style = "";
             td[i].style.fontSize = fonte+"px";
         }
     }
