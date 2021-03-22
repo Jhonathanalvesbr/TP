dbb2380137507fcbf195d08e8eabb63bff5b8e4f
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2021-03-22 10:28:05 -0300

    Update tp.js

--- tp.js
+++ tp.js
@@ -37,14 +37,15 @@ setInterval(tp,1000);
 
 
 
-var t = 13;
+var t = 12;
 function fonte(){
     let tabela = document.getElementsByTagName("table");
     let tr = tabela[0].getElementsByTagName("tr");
     for(let x = 0; x < tr.length; x++){
     let td = tr[x].getElementsByTagName("td");
-        for(let i = 0; i < td.length; i++){td[i].style = "";
-            td[i].style.fontSize = t+"px";
+    	for(let i = 0; i < td.length; i++){
+	    td[i].style.removeProperty('max-height');
+	    td[i].style.fontSize = t+"px";
         }
     }
 }
